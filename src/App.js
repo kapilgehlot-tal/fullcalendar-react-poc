ecccdf776fc8c3aec5ae4401586371a0e460cee9
Kapil Gehlot <KapilG@talentica-all.com>
2019-12-13 16:10:07 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,71 @@
+import React from "react";
+import FullCalendar from "@fullcalendar/react";
+import dayGridPlugin from "@fullcalendar/daygrid";
+import timeGridPlugin from "@fullcalendar/timegrid";
+import interactionPlugin from "@fullcalendar/interaction"; // needed for dayClick
+
+import "./main.scss";
+
+export default class App extends React.Component {
+  calendarComponentRef = React.createRef();
+  state = {
+    calendarWeekends: true,
+    calendarEvents: [
+      // initial event data
+    ]
+  };
+
+  render() {
+    return (
+      <div className="demo-app">
+        <div className="demo-app-top">
+          <button onClick={this.toggleWeekends}>toggle weekends</button>&nbsp;
+          <button onClick={this.gotoPast}>go to a date in the past</button>
+        </div>
+        <div className="demo-app-calendar">
+          <FullCalendar
+            defaultView="dayGridMonth"
+            header={{
+              left: "prev,next today",
+              center: "title",
+              right: "dayGridMonth,timeGridWeek,timeGridDay,listWeek"
+              // right:"Add Strength Training."
+            }}
+            plugins={[dayGridPlugin, timeGridPlugin, interactionPlugin]}
+            ref={this.calendarComponentRef}
+            weekends={this.state.calendarWeekends}
+            events={this.state.calendarEvents}
+            dateClick={this.handleDateClick}
+            firstDay={1}
+          />
+        </div>
+      </div>
+    );
+  }
+
+  toggleWeekends = () => {
+    this.setState({
+      // update a property
+      calendarWeekends: !this.state.calendarWeekends
+    });
+  };
+
+  gotoPast = () => {
+    let calendarApi = this.calendarComponentRef.current.getApi();
+    calendarApi.gotoDate("2000-01-01"); // call a method on the Calendar object
+  };
+
+  handleDateClick = arg => {
+    //if (confirm("Would you like to add an event to " + arg.dateStr + " ?")) {
+    this.setState({
+      // add new event data
+      calendarEvents: this.state.calendarEvents.concat({
+        // creates a new array
+        title: "<h2>New Event</h2>",
+        start: arg.date,
+        allDay: arg.allDay
+      })
+    });
+    //}
+  };
+}
